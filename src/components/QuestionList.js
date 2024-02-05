ce159851b6cb1031c01d9f51bda8cd90c844fe5a
Priscilla-22 <priscillamw123@gmail.com>
2024-02-06 00:01:56 +0300

    feat: use delete method to delete questions from the list

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -10,14 +10,29 @@ function QuestionList() {
       .then((data) => setQuestions(data))
       .catch((error) => console.error('Error fetching questions:', error));
   }, []);
-  
+
+
+  function handleDeleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        setQuestions(questions.filter((question) => question.id !== id));
+      })
+      .catch((error) => console.error('Error deleting question:', error));
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>
         {/* display QuestionItem components here after fetching */}
         {questions.map((question) => (
-          <QuestionItem key={question.id} question={question} />
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={() => handleDeleteQuestion(question.id)}
+          />
         ))}
       </ul>
     </section>
